96cb82ac344aed81169874c5cb05743ad55eeb0d
bodia1132 <bodia.baran.o@gmail.com>
2022-07-14 16:00:28 +0300

    func

--- AnalogFunction/main.cpp
+++ AnalogFunction/main.cpp
@@ -1,90 +1,144 @@
 #include <iostream>
+#include <memory.h>
+#include <mm_malloc.h>
+#include <functional>
 
-struct Test
-{
-    double d;
-    int a;
-    int b;
-    int c;
-    char e;
-};
+template<typename...>
+class function;
 
-class Base
+template<typename Res, typename... Args>
+class function<Res(Args...)>
 {
 public:
-    Base()
+    function() :
+            invoke_(nullptr),
+            create_(nullptr),
+            delete_(nullptr),
+            data_ptr(nullptr),
+            data_size(0) {}
+
+    template<typename Functor>
+    function(Functor f) :
+            invoke_(reinterpret_cast<Invoke_ptr> (invokeF<Functor>)),
+            create_(reinterpret_cast<Create_ptr> (createF<Functor>)),
+            delete_(reinterpret_cast<Delete_ptr> (deleteF<Functor>)),
+            data_ptr(new char[sizeof(Functor)]),
+            data_size(sizeof(Functor))
     {
-        std::cout << "Base Const" << std::endl;
+        create_(data_ptr.get(), reinterpret_cast<char*>(&f));
     }
-    virtual void Goof() const
+    function(const function &func) :
+            invoke_(func.invoke_),
+            create_(func.create_),
+            delete_(func.delete_),
+            data_size(func.data_size)
     {
-        std::cout << "Base" << std::endl;
+        if(invoke_ != nullptr)
+        {
+            data_ptr.reset(new char[data_size]);
+            create_(data_ptr.get(), func.data_ptr.get());
+        }
     }
-    virtual ~Base()
+
+    Res operator()(Args&&... args)
     {
-        std::cout << "Base Destr" << std::endl;
+        return invoke_(data_ptr.get(), std::forward<Args>(args)...);
+    }
+
+    ~function()
+    {
+        if(data_ptr != nullptr)
+        {
+            delete_(data_ptr.get());
+        }
+    }
+
+private:
+    using Invoke_ptr = Res(*)(void*, Args&&...);
+    using Create_ptr = void(*)(void*, void*);
+    using Delete_ptr = void(*)(void*);
+
+    Invoke_ptr invoke_;
+    Create_ptr create_;
+    Delete_ptr delete_;
+
+    std::unique_ptr<char[]> data_ptr;
+    size_t data_size;
+
+    template<typename Functor>
+    static Res invokeF(Functor *f, Args&&... args)
+    {
+        return (*f)(std::forward<Args>(args)...);
+    }
+
+    template<typename Functor>
+    static void createF(Functor *dst, Functor *src)
+    {
+        new (dst) Functor(*src);
+    }
+
+    template<typename Functor>
+    static void deleteF(Functor *f)
+    {
+        f->~Functor();
     }
 };
 
-class Enemy : public Base
+class A
 {
 public:
-    Enemy()
-    {
-        std::cout << "Enemy Const" << std::endl;
-    }
-    virtual void Goof() const override
-    {
-        std::cout << "Enemy" << std::endl;
-    }
-    virtual ~Enemy()
-    {
-        std::cout << "Enemy Destr" << std::endl;
-    }
+    A() = default;
 };
 
-class Player : public Base
+class B : public A
 {
 public:
-    Player()
-    {
-        std::cout << "Player Const" << std::endl;
-    }
-    virtual void Goof() const override
-    {
-        std::cout << "Player" << std::endl;
-    }
-    virtual ~Player()
-    {
-        std::cout << "Player Destr" << std::endl;
-    }
+    B() = default;
 };
 
 int main()
 {
-/*    Test test = {11.2, 62, 33, 8, 102};
+    int i = 0;
 
-    int i = sizeof(Test) / sizeof(int);
-    int *ptr = reinterpret_cast<int*>(&test);
+    auto l = [] (std::string &a) { return a; };
 
-    for(int j = 0; j < i; j++)
+    std::function<std::string(std::string&)> d = [] (std::string &a) -> decltype(auto) { return a; };
+
+    std::string alex = "alex";
+
+    std::cout << d(alex) << std::endl;
+
+    std::cout << l(alex) << std::endl;
+
+    auto fn = [i](std::string const& s) mutable
     {
-        std::cout << ptr[j] << std::endl;
-    }*/
+        std::cout << ++i << ". " << s << std::endl;
+    };
+    fn("first");                                   // 1. first
+    fn("second");                                  // 2. second
 
-    Player *player = new Player();
+    // construct from lambda
+    function<void(std::string const&)> f(fn);
+    f("third");                                    // 3. third
 
-    Base *ActualPlayer = player;
+    // copy from another function
+    function<void(std::string const&)> g(f);
+    f("forth - f");                                // 4. forth - f
+    g("forth - g");                                // 4. forth - g
 
-    Base *ActualEnemy = new Enemy();
+    // capture and copy non-trivial types like std::string
+    std::string x("xxxx");
+    function<void()> h([x]() { std::cout << x << std::endl; });
+    h();
 
-    Player *p = static_cast<Player*>(ActualEnemy);
-    Player *p1 = (Player*)ActualPlayer;
+    function<void()> k(h);
 
 
-    if(p && p1)
-    {
-        std::cout << "Laex" << std::endl;
-    }
+    A a;
+    B b;
+
+    A *c = static_cast<B*>(&b);
+
     return 0;
+
 }
